6d5a2adbe60cea594b62b46cd98c90b88c80fbd6
redflaggpl <edison.galindo@gmail.com>
2014-12-09 20:24:24 -0500

    client on bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,19 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+ 
+  Client.findOrCreate({"name" : "netbangers-web"},
+  {
+    "name" : "netbangers-web",
+    "redirectURI" : "http://localhost:1338",
+    "trusted" : true,
+    "clientId" : "9F7B4M4HO1",
+    "clientSecret" : "1lZFK0Qy4aioephpFypzaS5oZtx221"
+	}, function(err, client){
+		if(err) console.log(err);
+		else console.log(client);
+	});
+
   cb();
 };
